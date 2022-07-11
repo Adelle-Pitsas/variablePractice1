c28e4bc085cb9bd6c6709aab55ca24b557164e68
Adelle Pitsas <apitsas4@gmail.com>
2022-07-11 16:38:12 -0400

    Reassign variables and add print

--- variables1.js
+++ variables1.js
@@ -6,3 +6,9 @@ var isTired = true
 var isDriving = false
 var sentence = `${miles} to destination`
 console.log(sentence)
+
+town = "Manchester"
+console.log(isDriving)
+console.log(isTired)
+console.log(hours)
+console.log(car)
